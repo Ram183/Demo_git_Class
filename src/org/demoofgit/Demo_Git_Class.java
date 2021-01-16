d8d25f017078bac6a368076cdb1b124425c9153f
Ram183 <ramcrazy1992@gmail.com>
2021-01-16 20:07:13 +0530

    Adding a new Project

---
+++ src/org/demoofgit/Demo_Git_Class.java
@@ -0,0 +1,18 @@
+package org.demoofgit;
+
+public class Demo_Git_Class {
+
+	
+	public static void main(String[] args) {
+
+		System.out.println("GitHub");
+		System.out.println("GitHub");
+		System.out.println("GitHub");
+		System.out.println("GitHub");
+		System.out.println("Selenium");
+		System.out.println("Java");
+		System.out.println("Ruby");
+
+	}
+	
+}
