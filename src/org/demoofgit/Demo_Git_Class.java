a9d6c9b15c0091ce95ccc48a72e6f750c8f86e20
Ram183 <ramcrazy1992@gmail.com>
2021-01-16 21:04:49 +0530

    Modified new code as per Tl order

--- src/org/demoofgit/Demo_Git_Class.java
+++ src/org/demoofgit/Demo_Git_Class.java
@@ -6,9 +6,6 @@ public class Demo_Git_Class {
 	public static void main(String[] args) {
 
 		System.out.println("GitHub");
-		System.out.println("GitHub");
-		System.out.println("GitHub");
-		System.out.println("GitHub");
 		System.out.println("Selenium");
 		System.out.println("Java");
 		System.out.println("Ruby");
